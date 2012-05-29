819635b0020fd9f9beaa9b7eca7308490973e429
Paulo Alvarado <commonzenpython@gmail.com>
2012-05-28 23:43:42 -0400

    Converted to spaces

--- cms_media_gallery/utils.py
+++ cms_media_gallery/utils.py
@@ -1,10 +1,10 @@
 import base64
 
 def encode_string(string):
-	"""
-		Encrypts the string.
-	"""
-	return base64.b64encode(string)
+    """
+        Encrypts the string.
+    """
+    return base64.b64encode(string)
 
 def decode_string(string):
-	return base64.b64decode(string)
+    return base64.b64decode(string)
