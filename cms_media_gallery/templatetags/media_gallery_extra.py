c516f12561fabe7ebf5ba13da91e66309f943811
Paulo Alvarado <commonzenpython@gmail.com>
2012-06-04 20:56:21 -0400

    Added custom templatetags

---
+++ cms_media_gallery/templatetags/media_gallery_extra.py
@@ -0,0 +1,12 @@
+import os
+
+from django import template
+
+
+register = template.Library()
+
+
+# Thanks to rz. http://stackoverflow.com/a/4046508/389453
+@register.filter
+def filename(value):
+    return os.path.basename(value.file.name)
